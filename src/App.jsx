556882216d7d0b454dc6a8c284ba05469e45db19
Brett Grashorn <brettgrashorn@gmail.com>
2024-06-06 22:55:44 -0500

    Day 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
 import ViewAllJobs from './components/ViewAllJobs'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+);
+
+
 const App = () => {
 // JavaScript code can go here to return element in component
-  return (
-    // has to be wrapped in a single element
-    <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
+  return ( <RouterProvider router={router} /> )
 
-    </>
-    
-  )
 }
 
 export default App
