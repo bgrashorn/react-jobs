557e296d934b23cfabf2379239f8bf0ab89ceeec
Brett Grashorn <brettgrashorn@gmail.com>
2024-09-06 21:12:31 -0400

    2:41 - Browse job error

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -9,7 +9,7 @@ const JobListing = ({ job }) => {
 
   let description = job.description;
   if (!showFullDescription) {
-    description = description.substring(0, 90) + '...';
+    description = description.substring(0, 90);
   }
 
 
@@ -24,9 +24,7 @@ const JobListing = ({ job }) => {
               <div className="mb-5">
                 { description }
               </div>
-
               <button onClick={() => setShowFullDescription((prevState) => !prevState) } className="text-indigo-500 mb-5 hover:text-indigo-600">{showFullDescription ? 'Less': 'More'}</button>
-
               <h3 className="text-indigo-500 mb-2">{ job.salary }/ Year</h3>
 
               <div className="border border-gray-100 mb-5"></div>
